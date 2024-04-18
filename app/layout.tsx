f324e73690b6c150ed446846d237644e0a96feac
aryanploxxx <aryangupta8291@gmail.com>
2024-04-19 01:38:29 +0530

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Room from "./Room";
 
 export const metadata = {
-  title: "Figma Clone",
+  title: "Collab Design",
   description:
-    "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
+    "A minimalist design environment using fabric.js and Liveblocks for realtime collaboration",
 };
 
 const workSans = Work_Sans({
